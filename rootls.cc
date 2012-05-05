6a7364a28cc94ebc2e5335ff25755e29f411b0e9
Suvayu Ali (ThinkPad) <fatkasuvayu+linux@gmail.com>
2012-05-05 18:19:31 +0200

    Recursively browse and list directories

--- rootls.cc
+++ rootls.cc
@@ -5,7 +5,27 @@
 #include <boost/foreach.hpp>
 
 #include <TFile.h>
+#include <TCollection.h>
+#include <TKey.h>
+#include <TClass.h>
 
+
+/** 
+ * Recursively browse and list directories
+ *
+ * @param dir Directory to list
+ */
+void browse_dir(TDirectory *dir);
+
+
+/** 
+ * Take ROOT files and input
+ *
+ * @param argc Argument count
+ * @param argv ROOT filenames
+ *
+ * @return Exit status
+ */
 int main(int argc, char * argv[])
 {
   if (argc == 1) {
@@ -13,7 +33,7 @@ int main(int argc, char * argv[])
 	      << std::endl << std::endl;
     std::cout << "Usage: " << argv[0] << " <ROOT file(s)>"
 	      << std::endl << std::endl;
-    std::cout << "   List contents of the ROOT file."
+    std::cout << "   List contents of the ROOT file(s) recursively."
 	      << std::endl;
     return 1;
   }
@@ -24,9 +44,28 @@ int main(int argc, char * argv[])
   }
 
   BOOST_FOREACH(std::string filename, files) {
-    TFile rfile(filename.c_str(), "read");
-    rfile.ls();
-    std::cout << std::endl;
+    TFile *rfile = new TFile(filename.c_str(), "read");
+    browse_dir(rfile);
+    delete rfile;
   }
   return 0;
+}
+
+
+void browse_dir(TDirectory * dir)
+{
+  // list directory contents
+  dir->ls();
+  std::cout << std::endl;
+
+  // check for subdirectories
+  TIter keylist(dir->GetListOfKeys());
+  TKey *key;
+  while ((key = dynamic_cast<TKey*>(keylist()))) {
+    TClass *objClass = TClass::GetClass(key->GetClassName());
+    if (objClass->InheritsFrom(TDirectory::Class())) {
+      browse_dir(static_cast<TDirectory*>
+		 (key->ReadObjectAny(TDirectory::Class())));
+    }
+  }
 }
