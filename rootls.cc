51dd7cead837405445a4af5d35269061e72e8acc
Suvayu Ali (ThinkPad) <fatkasuvayu+linux@gmail.com>
2012-05-06 12:28:02 +0200

    Make recurse optional

--- rootls.cc
+++ rootls.cc
@@ -10,19 +10,20 @@
 #include <TClass.h>
 
 
-/** 
- * Recursively browse and list directories
+/**
+ * Browse and list directories. If recurse is true, list recursively.
  *
  * @param dir Directory to list
+ * @param recurse Whether to recursively list directories
  */
-void browse_dir(TDirectory *dir);
+void browse_dir(TDirectory *dir, bool recurse);
 
 
 /** 
  * Take ROOT files and input
  *
  * @param argc Argument count
- * @param argv ROOT filenames
+ * @param argv Options and ROOT filenames
  *
  * @return Exit status
  */
@@ -38,34 +39,60 @@ int main(int argc, char * argv[])
     return 1;
   }
 
-  std::vector<std::string> files;
+  // get args
+  std::vector<std::string> arguments;
   for (int i = 1; i < argc; ++i) {
-    files.push_back(argv[i]);
+    arguments.push_back(argv[i]);
   }
 
+  // get opts
+  bool recurse(false);
+  for (std::vector<std::string>::iterator argItr = arguments.begin();
+       argItr != arguments.end(); ++argItr) {
+    if (*argItr == "-r") {
+      recurse = true;
+      arguments.erase(argItr);
+      break;
+    }
+  }
+
+  // get filenames
+  std::vector<std::string> files;
+  BOOST_FOREACH(std::string argv, arguments) {
+    files.push_back(argv);
+  }
+
+  if (files.empty()) {
+    std::cout << "ERROR: No ROOT files provided! " << std::endl;
+    return 2;
+  }
+
+  // list files
   BOOST_FOREACH(std::string filename, files) {
     TFile *rfile = new TFile(filename.c_str(), "read");
-    browse_dir(rfile);
+    browse_dir(rfile, recurse);
     delete rfile;
   }
   return 0;
 }
 
 
-void browse_dir(TDirectory * dir)
+void browse_dir(TDirectory * dir, bool recurse)
 {
   // list directory contents
   dir->ls();
   std::cout << std::endl;
 
   // check for subdirectories
-  TIter keylist(dir->GetListOfKeys());
-  TKey *key;
-  while ((key = dynamic_cast<TKey*>(keylist()))) {
-    TClass *objClass = TClass::GetClass(key->GetClassName());
-    if (objClass->InheritsFrom(TDirectory::Class())) {
-      browse_dir(static_cast<TDirectory*>
-		 (key->ReadObjectAny(TDirectory::Class())));
+  if (recurse) {
+    TIter keylist(dir->GetListOfKeys());
+    TKey *key;
+    while ((key = dynamic_cast<TKey*>(keylist()))) {
+      TClass *objClass = TClass::GetClass(key->GetClassName());
+      if (objClass->InheritsFrom(TDirectory::Class())) {
+	browse_dir(static_cast<TDirectory*>
+		   (key->ReadObjectAny(TDirectory::Class())), recurse);
+      }
     }
   }
 }
