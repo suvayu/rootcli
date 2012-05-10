b66dce2133caaaf9a67bb98b8b9b262c3cf0b011
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2012-05-11 00:45:48 +0200

    Add more complicated regex to test

--- test.cc
+++ test.cc
@@ -10,24 +10,24 @@
 
 int main()
 {
-  TFile *file = new TFile("../VeloMoniGui/histo-check-Vetra_histos-2011-2012.root", "read");
+  TFile *file = new TFile("../VeloMoniGui/all-histos-Vetra_histos-2011-2012.root", "read");
   std::vector<TH2D*> histvec;
 
   Hist::HistReader reader1(file);
   reader1.getHistVec("Overview.*", histvec);
 
   Hist::HistReader reader2(file, "Velo/PCNErrorMon");
-  reader2.getHistVec("hperBeetle.*", histvec);
+  reader2.getHistVec("hperBeetle.*_104_(4|5|6|7)", histvec);
 
   BOOST_FOREACH(TH2D* hist, histvec) {
-    hist->Print("all");
+    hist->Print();
   }
 
   Hist::HistReader reader3(file, "Velo/PCNErrorMon");
   TH2D *hist = NULL;
   reader3.getHist("hBeetleMap", hist);
 
-  hist->Print("all");
+  hist->Print();
 
   return 0;
 }
